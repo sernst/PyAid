526ff7216b435f9fafd6eaa1bbd248915970d727
sernst <swernst@gmail.com>
2014-10-06 11:00:54 -0500

    As low-level screen resolution and HiDPI scaling query functions.

--- src/pyaid/OsUtils.py
+++ src/pyaid/OsUtils.py
@@ -1,9 +1,15 @@
 # OsUtils.py
-# (C)2013
+# (C)2013-2014
 # Scott Ernst
 
 import os
 import sys
+import re
+
+# AS AVAILABLE: import AppKit
+# AS AVAILABLE: import ctypes
+
+# AS NEEDED: from pyaid.system.SystemUtils import SystemUtils
 
 #___________________________________________________________________________________________________ OsUtils
 class OsUtils(object):
@@ -59,3 +65,88 @@ class OsUtils(object):
         ctypes.windll.shell32.SHGetFolderPathW(0, CSIDL_PERSONAL, 0, SHGFP_TYPE_CURRENT, buf)
         cls._WIN_DOCS_PATH = buf.value
         return cls._WIN_DOCS_PATH
+
+#___________________________________________________________________________________________________ isHighDpiScaledScreen
+    @classmethod
+    def isHighDpiScaledScreen(cls, index =-1):
+        if cls.isMac():
+            result = cls._getOsxDisplayInfo()
+            if result['code'] or not 'out' in result:
+                return None
+
+            if index < 0:
+                index = 0
+            screenIndex = 0
+            for line in result['out'].split(u'\n'):
+                line = line.strip().lower()
+                if not line.startswith('retina:'):
+                    continue
+                if screenIndex != index:
+                    continue
+
+                return line.split(':')[-1].strip().startswith('y')
+            return True
+
+        if cls.isWindows():
+            return False
+
+#___________________________________________________________________________________________________ getScreenResolution
+    @classmethod
+    def getScreenResolution(cls, index =-1):
+
+        #-------------------------------------------------------------------------------------------
+        if cls.isMac():
+            try:
+                import AppKit
+                from AppKit import NSScreen
+
+                if index == -1:
+                    return int(NSScreen.mainScreen().frame().size.width), \
+                           int(NSScreen.mainScreen().frame().size.height)
+
+                i = 0
+                for screen in AppKit.NSScreen.screens():
+                    if i != index:
+                        continue
+                    return screen.frame().size.width, screen.frame().size.height
+                return None
+            except Exception, err:
+                pass
+
+            result = cls._getOsxDisplayInfo()
+            if result['code'] or not 'out' in result:
+                return None
+
+            if index < 0:
+                index = 0
+            screenIndex = 0
+            for line in result['out'].split(u'\n'):
+                line = line.strip().lower()
+                if not line.startswith('resolution:'):
+                    continue
+                if screenIndex != index:
+                    continue
+
+                result = re.search(r'(?P<width>[0-9]{3,})[^0-9]+(?P<height>[0-9]{3,})', line)
+                return int(result.group('width')), int(result.group('height'))
+            return None
+
+        #-------------------------------------------------------------------------------------------
+        if cls.isWindows():
+            try:
+                import ctypes
+            except Exception, err:
+                return None
+            user32 = ctypes.windll.user32
+            return int(user32.GetSystemMetrics(0)), int(user32.GetSystemMetrics(1))
+
+
+
+#===================================================================================================
+#                                                                               P R O T E C T E D
+
+#___________________________________________________________________________________________________ _getOsxDisplayInfo
+    @classmethod
+    def _getOsxDisplayInfo(cls):
+        from pyaid.system.SystemUtils import SystemUtils
+        return SystemUtils.executeCommand(['system_profiler', 'SPDisplaysDataType'])
